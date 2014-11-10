e0bcb6ef03e21eee6a0bc50f00296b5afcfba2b0
afonso-praca <afonsoinfo@gmail.com>
2014-11-10 16:42:13 -0200

    atualiza karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,42 @@
-files = [
-	JASMINE,
-	JASMINE_ADAPTER,
-	'build/lib/bower_components/jquery/jquery.js',
-	'build/lib/bower_components/underscore/underscore.js',
-	'build/lib/bower_components/bootstrap/bootstrap.js',
-	'build/js/vtex-message.js',
-	'build/spec/helpers/jasmine-jquery.js',
-	'build/spec/helpers/mock-ajax.js',
-	{
-		pattern: 'build/spec/fixtures/**/*.*',
-		watched: true,
-		included: false,
-		served: true
-	},
-	'build/spec/*.js'
-];
-browsers = [
-	'PhantomJS'
-];
+//files = [
+//	JASMINE,
+//	JASMINE_ADAPTER,
+//	'build/lib/bower_components/jquery/jquery.js',
+//	'build/lib/bower_components/underscore/underscore.js',
+//	'build/lib/bower_components/bootstrap/bootstrap.js',
+//	'build/js/vtex-message.js',
+//	'build/spec/helpers/jasmine-jquery.js',
+//	'build/spec/helpers/mock-ajax.js',
+//	{
+//		pattern: 'build/spec/fixtures/**/*.*',
+//		watched: true,
+//		included: false,
+//		served: true
+//	},
+//	'build/spec/*.js'
+//];
+
+
+module.exports = function(config) {
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine'],
+		browsers: [ 'PhantomJS'],
+		port: 9876,
+		files: [
+			'build/front-messages-ui/lib/bower_components/jquery/jquery.js',
+			'build/front-messages-ui/lib/bower_components/underscore/underscore.js',
+			'build/front-messages-ui/lib/bower_components/bootstrap/bootstrap.js',
+			'build/front-messages-ui/script/vtex-message.js',
+			'build/front-messages-ui/spec/helpers/jasmine-jquery.js',
+			'build/front-messages-ui/spec/helpers/mock-ajax.js',
+			{
+				pattern: 'build/front-messages-ui/spec/fixtures/**/*.*',
+				watched: true,
+				included: false,
+				served: true
+			},
+			'build/front-messages-ui/spec/*.js'
+		]
+	});
+};
