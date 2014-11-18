e4c59b423406f5f038925b568c74e18095a3e6bd
afonso-praca <afonsoinfo@gmail.com>
2014-11-18 18:53:52 -0200

    remove configurações antigas do karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,3 @@
-//files = [
-//	JASMINE,
-//	JASMINE_ADAPTER,
-//	'build/lib/bower_components/jquery/jquery.js',
-//	'build/lib/bower_components/underscore/underscore.js',
-//	'build/lib/bower_components/bootstrap/bootstrap.js',
-//	'build/js/vtex-message.js',
-//	'build/spec/helpers/jasmine-jquery.js',
-//	'build/spec/helpers/mock-ajax.js',
-//	{
-//		pattern: 'build/spec/fixtures/**/*.*',
-//		watched: true,
-//		included: false,
-//		served: true
-//	},
-//	'build/spec/*.js'
-//];
-
-
 module.exports = function(config) {
 	config.set({
 		basePath: '',
